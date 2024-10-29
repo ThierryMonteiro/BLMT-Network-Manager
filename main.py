18b190afd01e2eb64bfea3672c3066d14ffc19c7
Luis Siqueri <luissiquerigg@gmail.com>
2024-10-29 02:39:58 -0300

    feat: endereços de rede automáticos e salvar jsons

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import detectHosts
 import OwnInformation
+import aux
 
 # OwnInformation pega as informações de rede do prórpio host
 #TODO fazer pegar o endereço MAC
 ownInformation = OwnInformation.managerDevice()
 ownInformation.print_table()
+IPs = ownInformation.getIP()
+masks = ownInformation.getNetworkRange()
 
+informations = {}
+for i in range(len(IPs)):
+    detectHosts = detectHosts.detectHosts(IPs[i], masks[i])
+    detectHosts.printTable()
+    informations = detectHosts.getInformations()
 
-# detectHosts pega as informações de hosts na rede
-detectHosts = detectHosts.detectHosts()
-detectHosts.display_by_subnet("10.1.1.0/24")
-
+jsonFile = "./output.json"
+data = aux.loadData(jsonFile)
+aux.addContent(data, informations)
+aux.saveData(jsonFile, data)
