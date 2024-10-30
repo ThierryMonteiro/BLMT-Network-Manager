b2c46ce76317b2d29e41481242153144aaa1e0b4
Thierry Monteiro <thierrywmon@gmail.com>
2024-10-29 23:48:43 -0400

    feat: flag de função para cada IP da rede feat: Verificação de Router

--- detectHosts.py
+++ detectHosts.py
@@ -1,7 +1,12 @@
 from prettytable import PrettyTable
 from scapy.all import ARP, Ether, srp
+
+import asyncio
+from pysnmp.hlapi.v3arch.asyncio import *
+
 import aux
 from datetime import datetime
+
 
 class detectHosts:
 
@@ -26,15 +31,26 @@ class detectHosts:
     def printTable(self):
         print(self.table)
 
-    def getInformations(self):
+    async def getInformations(self):
         date = datetime.now().strftime("%d/%m/%Y %H:%M")
         informations = []
         clients = self.bySubnet()
-        for ip, mac in clients.items():
+
+        #todo verificar se é um roteador só se AnwserSNMPV2 retornar True
+        tasks = [self.isRouter(ip) for ip in clients.keys()]
+        router_checks = await asyncio.gather(*tasks)
+
+        # @LUIS: cara eu tive que dar uma mexida a mais aqui mal eu.
+        # Coloquei Função: como campo, pois um ativo de rede pode ser alem de roteador um
+        # firewall, nat, etc. Então acho que seria interessante ter essa informação como uma array de strings dentro de
+        # um campo chamado genérico.
+
+        for (ip, mac), router_status in zip(clients.items(), router_checks):
             informations.append({
                 "IP": ip,
                 "Mac Address": mac,
-                "Owner": aux.ouiExtractor(mac)
+                "Owner": aux.ouiExtractor(mac),
+                "Tipo": router_status,
             })
         toJson = {"Date": date, "Devices": informations}
         return toJson
@@ -46,4 +62,61 @@ class detectHosts:
             self.table.add_row([ip, mac])
 
 
-    #todo adicionar o campo que o raniery pediu de se é novo, antigo, etc
+    async def isRouter(self, ip):
+
+        community = 'public'
+        oid_route_table = '1.3.6.1.2.1.4.21'  # ipRouteTable
+
+        transport_target = await UdpTransportTarget.create((ip, 161))
+
+        snmpEngine = SnmpEngine()
+
+        # Chamar get_cmd corretamente
+        errorIndication, errorStatus, errorIndex, varBinds = await get_cmd(
+            snmpEngine,
+            CommunityData(community),
+            transport_target,
+            ContextData(),
+            ObjectType(ObjectIdentity(oid_route_table))
+        )
+
+        snmpEngine.close_dispatcher()
+        if errorIndication:
+            pass
+
+        elif errorStatus:
+            pass
+        else:
+            print(ip)
+            return "Router"
+
+        return "Not Router"
+
+
+
+    async def AwnserSNMPV2(self, ip):
+
+        # teste 1: verificar se usa um SNMP
+
+        snmpEngine = SnmpEngine()
+
+        # First SNMP request to get system description
+        iterator = get_cmd(
+            snmpEngine,
+            CommunityData("public", mpModel=0),
+            await UdpTransportTarget.create((ip, 161)),
+            ContextData(),
+            ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+        )
+
+        errorIndication, errorStatus, errorIndex, varBinds = await iterator
+
+        if errorIndication:
+            pass
+
+        elif errorStatus:
+            pass
+        else:
+            return True
+
+        return False
--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import detectHosts
 import OwnInformation
 import aux
+import asyncio
 
 # OwnInformation pega as informações de rede do prórpio host
-#TODO fazer pegar o endereço MAC
-ownInformation = OwnInformation.managerDevice()
-ownInformation.print_table()
-IPs = ownInformation.getIP()
-masks = ownInformation.getNetworkRange()
 
-informations = {}
-for i in range(len(IPs)):
-    detectHosts = detectHosts.detectHosts(IPs[i], masks[i])
-    detectHosts.printTable()
-    informations = detectHosts.getInformations()
+async def main():
 
-jsonFile = "./output.json"
-data = aux.loadData(jsonFile)
-aux.addContent(data, informations)
-aux.saveData(jsonFile, data)
+    ownInformation = OwnInformation.managerDevice()
+    ownInformation.print_table()
+    IPs = ownInformation.getIP()
+    masks = ownInformation.getNetworkRange()
+
+    informations_list = []
+
+
+    for i in range(len(IPs)):
+        if i == 0:
+            detectedHosts = detectHosts.detectHosts(IPs[i], masks[i])
+            detectedHosts.printTable()
+            informations_list.append(await detectedHosts.getInformations())
+
+    print("Informações dos hosts detectados:")
+
+    jsonFile = "./output.json"
+    data = aux.loadData(jsonFile)
+    for info in informations_list:
+        aux.addContent(data, info)
+    aux.saveData(jsonFile, data)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
